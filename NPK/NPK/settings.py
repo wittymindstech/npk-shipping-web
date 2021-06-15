7bea9f328db98f1935ff90708ab1168a8e7a19b9
store <jeet8halder@gmail.com>
2021-06-15 09:20:49 +0530

    User login and register
    Updated with working user authentication

--- NPK/NPK/settings.py
+++ NPK/NPK/settings.py
@@ -126,6 +126,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Login Settings
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
+LOGOUT_URL = '/accounts/logout/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
